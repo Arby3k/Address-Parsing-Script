c1c8a1cdb5a0e34035a4cbf08279dfd77fc5e7c2
Arbab Ahmed <arby3k@gmail.com>
2021-01-15 19:31:28 -0800

    Cleaned up loop to load

--- src/script.py
+++ src/script.py
@@ -1,15 +1,15 @@
+From dotenv import load_dotenv
+load_dotenv()
+
 import pandas as pd
 from tkinter.filedialog import askopenfilename
 filename = askopenfilename()
 
-
-x=0
 months = ['JAN', 'FEB', 'MAR', 'APR', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-excelSheet = [0,0,0,0,0,0,0,0,0,0,0,0]
-ColAddress = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-for month in months:
-    excelSheet[x] = pd.read_excel (filename, sheet_name = month)
-    ColAddress[x] = pd.DataFrame(excelSheet[x], columns= ['ADDRESS FOUND'])
-    x = x+1
+excelSheet = []
+ColAddress = []
 
+for i, month in enumerate(months):
+    excelSheet.append( pd.read_excel (filename, sheet_name = month) )
+    ColAddress.append( pd.DataFrame(excelSheet[i], columns= ['ADDRESS FOUND']) )
