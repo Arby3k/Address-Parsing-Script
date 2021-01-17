cc3c2a4b666b36076b293c193d6ac610ea7e3470
Arbab Ahmed <arby3k@gmail.com>
2021-01-16 21:52:53 -0800

    Removed Export to Excel functionality
    Switching gears to google API functionality, will work on exporting once data has been parsed.

--- src/script.py
+++ src/script.py
@@ -6,7 +6,7 @@ KEY = os.getenv("api-key")
 
 #Pandas, and Excel enviroment import
 import pandas as pd
-from openpyxl import load_workbook
+import openpyxl as op
 
 #File managment Import
 from tkinter import filedialog
@@ -17,21 +17,15 @@ filename = filedialog.askopenfilename()
 months = ['JAN', 'FEB', 'MAR', 'APR', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 
 
-book = load_workbook(filename)
-writer = pd.ExcelWriter(filename, engine = 'openpyxl')
-writer.book = book
 
-for month in months:
-    excelSheet = pd.read_excel(writer, sheet_name = month)
+for i, month in enumerate(months):
+    excelSheet = pd.read_excel(filename, sheet_name = month)
     addressCol = pd.DataFrame(excelSheet, columns= ['ADDRESS FOUND']) 
-    
+    addressCol.dropna(inplace=True)
     #Do stuff
-
-    addressCol.to_excel(writer, sheet_name = month, columns= ['ADDRESS FOUND'], index = False )
-
-
-writer.save()
-writer.close()
+    print(addressCol)
+    
+    #addressCol.to_excel(writer, sheet_name = month, columns= ['ADDRESS FOUND'], index = False )
 
 # psedoCode
 
