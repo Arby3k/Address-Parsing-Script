e1f148b501686887ca8b73a562d19885d3f752c3
Arbab Ahmed <arby3k@gmail.com>
2021-01-17 19:41:31 -0800

    Address parsing functioning

--- src/script.py
+++ src/script.py
@@ -12,6 +12,7 @@ gmaps = googlemaps.Client(key=KEY)
 import pandas as pd
 import openpyxl as op
 
+
 #File managment Import
 from tkinter import filedialog
 filename = filedialog.askopenfilename()
@@ -21,14 +22,19 @@ filename = filedialog.askopenfilename()
 months = ['JAN']#, 'FEB', 'MAR', 'APR', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 
 
-
 for i, month in enumerate(months):
     excelSheet = pd.read_excel(filename, sheet_name = month)
     addressCol = pd.DataFrame(excelSheet, columns= ['ADDRESS FOUND']) 
     addressCol.dropna(inplace=True)
     rowCount = addressCol.shape[0]
-    print(addressCol)
-    print(type(rowCount))
+    #print(addressCol)
+    print(addressCol.iloc[0][0])
+    addStr = addressCol.iloc[0][0]
+    geocode_result = gmaps.geocode(addStr)
+    fullAddress = geocode_result[0]["formatted_address"]
+    print(fullAddress)
+
+
     # for 
     #     geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
